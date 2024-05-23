b6e9529bf8ad451fa99e04d39efb649b7485c070
kennyow <kennyow86@gmail.com>
2024-05-23 14:41:24 -0300

    Remove api's

--- accessing_api/topic_headlines_country.py
+++ accessing_api/topic_headlines_country.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_news (country, api_key = '0cb6bbd15c2d415ebba7f533b337b159'):
+def get_news (country, api_key = 'xxxxxxxxxxxxx'):
     url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
     r = requests.get(url)
     content = r.json()
--- accessing_api/weather_forecast.py
+++ accessing_api/weather_forecast.py
@@ -2,7 +2,7 @@ import requests
 
 
 
-def get_weather(city, units='metric', api_key = '56c91a4840c19a1c02cebc5078d2a8ed'):
+def get_weather(city, units='metric', api_key = 'xxxxxxxxxxxxxx'):
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}'
     r = requests.get(url)
     content = r.json()
